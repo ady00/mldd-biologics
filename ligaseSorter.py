b804262a87e4aaafd8a0b57cd6f7def03ba8e6f3
Random person <44148908+TryhardCoder2@users.noreply.github.com>
2022-07-21 22:57:19 -0500

    Create ligaseSorter.py

---
+++ ligaseSorter.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+Fdirectory = os.fsencode("C:\ASDRP\ML-DD\PDB Sort\FASTA dataset - Copy") #Set this to whatever directory has the files
+Rdirectory = os.fsencode("C:\ASDRP\ML-DD\PDB Sort\Sorted Sequences") #Set this to the directory where you want the correct files to be stored
+goodFiles = []
+count = 0
+
+for file in os.listdir(Fdirectory):
+    absolutePath = os.path.join(Fdirectory, file)
+    with open(absolutePath) as theFile:
+        text = theFile.readline().rstrip()
+        if "ligase" in text:
+            goodFiles.append(file.decode())
+            shutil.copy(absolutePath, Rdirectory)
+            count +=1
+print(goodFiles)
+print(count)
+#with open("C:\ASDRP\ML-DD\PDB Sort\sequenceNames.txt", 'w') as f: #This part is if you want the names of the sequences as a text file if you are planning other stuff. If you do, set the directory to where you want the text file to be, and uncomment these lines
+#    for seq in goodFiles:
+#        f.write(seq)
+        
+        
